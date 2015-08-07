ba2368201a4136d542f8b0829794544d9766727c
Suzanne Wang <suzanneswang@gmail.com>
2015-08-07 10:28:42 -0700

    [add] flux file architecture

--- app/client.js
+++ app/client.js
@@ -1,7 +1,7 @@
 'use strict';
 import React from 'react';
 import Router from 'react-router';
-import Routes from './components/routes';
+import Routes from './js/components/routes';
 
 Router.run(Routes, Router.HistoryLocation, function(Root) {
   React.render(<Root />, document.body);
