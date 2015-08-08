353bfbb6b6df47b9654153efb921fcd0115f12e8
Suzanne Wang <suzanneswang@gmail.com>
2015-08-07 17:09:58 -0700

    [refactor] include utils

--- app/js/actions/treeActionCreators.js
+++ app/js/actions/treeActionCreators.js
@@ -2,5 +2,6 @@
 
 var Dispatcher = require('../dispatcher/dispatcher');
 var Constants = require('../constants/constants');
+var Utils = require('../utils/utils');
 
 var ActionTypes = Constants.ActionTypes;
