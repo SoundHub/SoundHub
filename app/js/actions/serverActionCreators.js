aa76af827dfc9338a54851b51904e1ab2c95457c
Suzanne Wang <suzanneswang@gmail.com>
2015-08-07 17:08:43 -0700

    [add] flux architecture files

---
+++ app/js/actions/serverActionCreators.js
@@ -0,0 +1,6 @@
+// actions relating to server
+
+var Dispatcher = require('../dispatcher/dispatcher');
+var Constants = require('../constants/constants');
+
+var ActionTypes = Constants.ActionTypes;
---
+++ app/js/actions/userActionCreators.js
@@ -0,0 +1,6 @@
+// handles user/profile info
+
+var Dispatcher = require('../dispatcher/dispatcher');
+var Constants = require('../constants/constants');
+
+var ActionTypes = Constants.ActionTypes;
