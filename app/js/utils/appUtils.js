4cd5a10014553724bddeb5d642c3ae0100791544
Suzanne Wang <suzanneswang@gmail.com>
2015-08-08 10:24:27 -0700

    [feat] add fetch in appUtils

--- app/js/utils/appUtils.js
+++ app/js/utils/appUtils.js
@@ -1,7 +1,24 @@
-// contains calls to the server
+// central location for all communication with our api
 
-module.exports = {
+import fetch from 'whatwg-fetch';
 
-  // methods here
+export default {
+  get(url) {
+    return fetch(url, {
+      method: 'get'
+    })
+    .then((response) => {
+      console.log(response);
+    })
+  },
 
-};
+  post(url, body) {
+    return fetch(url, {
+      method: 'post',
+      body: body
+    })
+    .then((response) => {
+      console.log(response);
+    })
+  }
+}
