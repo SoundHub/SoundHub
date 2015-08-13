baf7669ae9dc147c44dcb8034cbdaa7bd1bd846d
Mikhail <mishnade@gmail.com>
2015-08-12 19:02:18 -0700

    fix (user actions): user actions mostly complete

--- app/js/actions/userActionCreators.js
+++ app/js/actions/userActionCreators.js
@@ -4,53 +4,62 @@ import Dispatcher from '../dispatcher/dispatcher';
 import Constants from '../constants/constants';
 import Utils from '../utils/utils';
 
-let ActionTypes = Constants.ActionTypes;
+let ActionType = Constants.ActionTypes;
 
 export default {
-  createUser (user) {
+  createUser (userName, password, email = "") {
+    let user = {
+      userName: userName,
+      password: password,
+      email: email
+    };
     Utils.post('/signup', user)
     .then((response) => {
       Dispatcher.dispatch({
-        type: ActionTypes.NEW_USER,
+        type: ActionType.NEW_USER,
         user: user
       });
       console.log('user created');
-    });
+    })
     .catch((err) => {
       console.error('signup failed ', err);
-    })
+    });
   },
 
   loginUser (userName, password) {
-    Utils.post('/login', userID, songID)
+    let user = {
+      userName: userName,
+      password: password
+
+    };
+    Utils.post('/login', user)
     .then((response) => {
       Dispatcher.dispatch({
         type: ActionType.LOGIN,
         message: 'Login successful',
-        songData: songData
+        user: user
       });
       console.log('logged in successfuly');
 
     })
     .catch((err) => {
-      console.error('login failed: ', err)
-    })
+      console.error('login failed: ', err);
+    });
   },
 
-  logoutUser (userName) {
-    Utils.post('/logout', userID, songID)
+  logoutUser (userID) {
+    Utils.post('/logout', userID)
     .then((response) => {
       Dispatcher.dispatch({
         type: ActionType.LOGOUT,
         message: 'Logout successful',
-        songData: songData
+        user: userID
       });
       console.log('logged out successfuly');
-
     })
     .catch((err) => {
-      console.error('logout failed: ', err)
-    })
+      console.error('logout failed: ', err);
+    });
   }
   
-}
+};
