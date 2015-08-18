5bce8ec6ab3d586d13d52b5573efe31186b48441
chuqidai <daichuqi@gmail.com>
2015-08-18 15:16:04 -0700

    feat(fav): completed

--- app/js/constants/constants.js
+++ app/js/constants/constants.js
@@ -19,7 +19,9 @@ let constants = {
       CREATE_FROM_FORKS: null,
       GET_USER_VOTES: null,
       NEW_SONG_VOTED: null,
-      CHANGE_IMG_URL:null
+      CHANGE_IMG_URL:null,
+      RECEIVE_ALL_FAV_SONGS:null,
+      ADD_FAV_SUCCESS:null
   })
 };
 
