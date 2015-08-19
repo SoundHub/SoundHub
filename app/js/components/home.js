ff05c7e4e9101889519447b3fbd3cb58368483d9
Mikhail <mishnade@gmail.com>
2015-08-19 12:32:10 -0700

    :godmode: feat(home): sorting now works

--- app/js/components/home.js
+++ app/js/components/home.js
@@ -87,7 +87,18 @@ class Home extends React.Component {
         <div className= "playerBox">
           <AudioPlayer song = {this.state.currentsong} mode = "home" />
         </div>
-          <SongList data = {this.state.songs.allSongs} switchSong={this.switchSong} page='home'/>
+          <SongList data = {this.state.songs.allSongs.sort(function(a, b) {
+            if (order === 'like') {
+              return b[order] - a[order];
+            }
+            else if (order = 'createdAt') {
+              let a_date = new Date(a.createdAt);
+              let b_date = new Date(b.createdAt);
+              console.log(a_date, b_date);
+              return b_date - a_date;
+            }
+          })} 
+          switchSong = {this.switchSong} />
       </div>
     );
   }
