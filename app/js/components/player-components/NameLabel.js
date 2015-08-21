1efcbf75e0a2f6d9cd087bc78dc8b0a71a593183
chuqidai <daichuqi@gmail.com>
2015-08-21 10:08:49 -0700

    fix(UI)home player and banner

--- app/js/components/player-components/NameLabel.js
+++ app/js/components/player-components/NameLabel.js
@@ -1,17 +1,10 @@
-var React = require('react/addons');
-
+import React from 'react';
 import {Label} from 'react-bootstrap';
 
 module.exports = React.createClass({
 	render: function() {
-    if(this.props.mode==='user'){
-      return (
-        <span className="userSongTitle">{this.props.title} </span>
-      );
-    }else{
       return (
         <span className="audio-name-label pull-left">{this.props.title} </span>
       );
-    }
 	}
 })
