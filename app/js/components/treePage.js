1c48911f9b6ff9b0db42f564f029bb29b7f4f922
jimofthenorth <james.larrenaga@gmail.com>
2015-08-24 15:04:09 -0700

    :bug:fix(treePage.js): closes #182, found duplicate entries for modal code on treePage.js and tree.js

--- app/js/components/treePage.js
+++ app/js/components/treePage.js
@@ -5,8 +5,6 @@ import D3Tree from './tree.js';
 import SongActions from '../actions/songActionCreators';
 import SongTreeStore from '../stores/songTreeStore';
 import Router from 'react-router';
-import ModalStore from '../stores/modalStore';
-import UserActionModal from './userActionModal';
 
 class Page extends React.Component {
   constructor() {
@@ -14,11 +12,10 @@ class Page extends React.Component {
     var query = window.location.pathname.split('/')[2];
     var rootId = query.split('&')[0];
     var uuid = query.split('&')[1];
-    SongActions.getSongTree({rootId: rootId});  //'/1/'  '/1/2/'
+    SongActions.getSongTree({rootId: rootId});
     this.state = {
       treeData: {},
-      uuid: uuid,
-      actionModalVisible: false
+      uuid: uuid
     }
 
     SongTreeStore.getTree();
@@ -26,24 +23,19 @@ class Page extends React.Component {
     this.componentWillMount = this.componentWillMount.bind(this);
     this.render = this.render.bind(this);
     this._onChange = this._onChange.bind(this);
-    this._onAction = this._onAction.bind(this);
-    this.closeActionModal = this.closeActionModal.bind(this);
   }
 
   componentWillMount() {
     SongTreeStore.addChangeListener(this._onChange);
-    ModalStore.addActionListener(this._onAction);
   }
 
   componentWillUnmount() {
     SongTreeStore.removeChangeListener(this._onChange);
-    ModalStore.removeActionListener(this._onAction);
   }
 
   render() {
     return (
       <div className="treePage ">
-        <UserActionModal show={this.state.actionModalVisible} message={this.state.actionMessage} onHide={this.closeActionModal} />
         <D3Tree treeData={this.state.treeData} uuid={this.state.uuid} />
       </div>
     );
@@ -53,16 +45,6 @@ class Page extends React.Component {
     this.setState({ treeData: SongTreeStore.getTree() });
   }
 
-  _onAction() {
-    this.setState({actionModalVisible: true, actionMessage: ModalStore.getActionMessage()})
-    setTimeout(() => {
-      this.closeActionModal();
-    }, 500)
-  }
-
-  closeActionModal() {
-    this.setState({actionModalVisible: false});
-  }
 }
 
 export default Page;
