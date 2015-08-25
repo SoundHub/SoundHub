73f1c013d1e1e7877090ea3b84ae8d4d830131cb
Suzanne Wang <suzanneswang@gmail.com>
2015-08-24 20:02:23 -0700

    test(jest): implement with payload mock

--- app/js/stores/__tests__/allSongStore-test.js
+++ app/js/stores/__tests__/allSongStore-test.js
@@ -1,19 +1,42 @@
 jest.dontMock('../allSongStore');
 jest.dontMock('object-assign');
 
+var Constants = require('../../constants/constants');
+
 describe('allSongStore', () => {
 
   var Dispatcher;
-  var SongStore;
+  var AllSongStore;
   var callback;
+  const ActionType = Constants.ActionTypes;
 
   beforeEach(() => {
     Dispatcher = require('../../dispatcher/dispatcher');
-    SongStore = require('../allSongStore');
+    AllSongStore = require('../allSongStore');
     callback = Dispatcher.register.mock.calls[0][0];
   });
 
   it('registers a callback with dispatcher', () => {
     expect(Dispatcher.register.mock.calls.length).toBe(1);
   });
+
+  it('receives songs to populate the front page', () => {
+    var payload = {
+      type: ActionType.RECEIVE_ALL_SONGS_SORTED,
+      songs: {
+        songs: [{
+          title: 'test1',
+          author: 'suzanne'
+        }, {
+          title: 'test2',
+          author: 'jenny'
+        }],
+        number: 1
+      }
+    }
+
+    callback(payload);
+    var all = AllSongStore.getAllSongs();
+    console.log(all);
+  })
 })
