c984d3b3ab0649086629b9d9b897de69c56db1a4
Suzanne Wang <suzanneswang@gmail.com>
2015-08-25 19:57:08 -0700

    test(stores): mock payload

--- app/js/stores/__tests__/allSongStore-test.js
+++ app/js/stores/__tests__/allSongStore-test.js
@@ -4,16 +4,31 @@ jest.dontMock('object-assign');
 describe('allSongStore', () => {
 
   var Dispatcher;
-  var SongStore;
+  var AllSongStore;
   var callback;
 
   beforeEach(() => {
     Dispatcher = require('../../dispatcher/dispatcher');
-    SongStore = require('../allSongStore');
+    AllSongStore = require('../allSongStore');
     callback = Dispatcher.register.mock.calls[0][0];
   });
 
   it('registers a callback with dispatcher', () => {
     expect(Dispatcher.register.mock.calls.length).toBe(1);
   });
+
+  it('should receive songs', () => {
+    var payload = {
+      type: 'RECEIVE_ALL_SONGS_SORTED',
+      songs: [{
+        title: 'test1'
+      }, {
+        title: 'test2'
+      }],
+      number: 2
+    }
+
+    callback(payload);
+    expect(AllSongStore.getSongNum()).toBe(2);
+  })
 })
